4b32a3935ecbd34e851c78ed16288f4609baa37b
Joseph KM <kuttz.dev@gmail.com>
2026-03-03 00:09:16 -0300

    Revert "fix: scoreboard spectator categorization for dead players (#42)" (#43)
    This reverts commit 620d3ff8e9749eae734e1e9832d233bb2ad754e9.

--- src/main/java/ar/ncode/plugin/ui/pages/ScoreBoardPage.java
+++ src/main/java/ar/ncode/plugin/ui/pages/ScoreBoardPage.java
@@ -75,7 +75,7 @@ public class ScoreBoardPage extends BasicCustomUIPage {
 				continue;
 			}
 
-			if (player.info().isSpectator() || player.info().getCurrentRoundRole() == null) {
+			if (player.info().getCurrentRoundRole() == null) {
 				result.spectators.add(player);
 				continue;
 			}
@@ -115,7 +115,6 @@ public class ScoreBoardPage extends BasicCustomUIPage {
 
 	@Override
 	public void build(@NonNullDecl UICommandBuilder builder) {
-		rowNumber = 1;
 		buildBaseScoreBoard(builder);
 
 		if (super.playerRef.getWorldUuid() == null) {
